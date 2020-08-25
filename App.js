a88f654ba4452fa4e20920eb5593969f5f407152
JinleeJeong <pjjr0118@gmail.com>
2020-08-25 14:15:25 +0900

    finished first RN App

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
 import React from 'react';
+import {Alert} from 'react-native';
 import Loading from './Loading';
+import Weather from './Weather';
 import * as Location from 'expo-location';
+import axios from 'axios';
+
 export default class extends React.Component {
 
+  state = {
+    isLoading: true,
+  }
+
+  getWeather = async(longitude, latitude) => {
+
+    const { data: {
+      main: {temp},
+      weather
+    } 
+  
+  } = await axios.get(
+    `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    )
+
+    this.setState({isLoading : false, temp, condition : weather[0].main});
+
+  }
+
   getLocation = async() => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      await Location.requestPermissionsAsync();
+
+      const {
+        coords : {longitude, latitude}
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(longitude, latitude);
+
+    } catch (error) {
+      Alert.alert("Can`t find you");
+    }
   }
   componentDidMount(){
     this.getLocation();
   }
   render() {
-      return (
-      <Loading/>
-      );
+    const {isLoading, temp, condition} = this.state;
+
+      return isLoading? <Loading/> : <Weather temp={Math.round(temp)} condition={condition}/>;
     }
 }
 
